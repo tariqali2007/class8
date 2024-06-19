cb5981bc5e193f3b55ba92e76e23e95f3c5367c9
tariqali2007 <tariqalimahar2007@gmail.com>
2024-06-19 19:24:23 +0500

    class8

---
+++ javaclass8/style6.js
@@ -0,0 +1,36 @@
+
+///////////////////array ;
+let arr = ["shaoib ", "abdullah", "wasif ", "waseem"];
+//  arr[0] = "wasif-"// not recommended
+  // arr.push("naveed") // array value add end
+  // arr.unshift("ali") // array first value added
+  //arr.pop()  // array last value remove 
+  // arr.shift() // array first value remove 
+// arr.length
+console.log(arr);
+//  console.log(arr.length);
+arr.splice(1, 2, "noman ", "asif ", "umair") // added and remove or replace
+console.log(arr);
+let b = arr.slice(2, 5)
+console.log(b);
+// console.log(b);
+// let a =  [1, 3, 4,5 , 7, 8, 8., 9, 0,];
+// console.log(a);
+// console.log(a.length);
+
+// var num = 77;
+// num = 77;
+// num = 99;
+
+// {
+//   var num = 55;
+// }
+
+// console.log(num);
+
+// function sum( a , b , c ) {
+//  return a + b + c;
+// }
+
+// let add = sum(991 , 99 , 18);
+// console.log(add);
